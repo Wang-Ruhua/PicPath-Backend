7cf2b62386e0adabd6ce4928204a41308c26eaad
Ruhua Wang <ruhuawan@usc.edu>
2023-03-16 17:15:02 -0700

    first commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.138.0.2:9200"
+	ES_USERNAME = "admin"
+	ES_PASSWORD = "project123"
+	GCS_BUCKET  = "goproject"
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"picpath/backend"
+	"picpath/handler"
+	"picpath/util"
+)
+
+func main() {
+	fmt.Println("started-service")
+
+	config, err := util.LoadApplicationConfig("conf", "deploy.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	backend.InitElasticsearchBackend(config.ElasticsearchConfig)
+	backend.InitGCSBackend(config.GCSConfig)
+
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter(config.TokenConfig)))
+}
